48c9d1c34326c8a6294490982b3550004613f0d7
alusiani <alusiani@0b3a14d8-1776-11df-914e-2594af177612>
2010-03-23 18:13:57 +0000

    plot measurements and averages in pdg_average.input

--- Common/bin/plotMeasurements.r
+++ Common/bin/plotMeasurements.r
@@ -5,6 +5,8 @@
 ## plot measurements in pdg_average.input
 ##
 
+library(plotrix)
+
 ## ////////////////////////////////////////////////////////////////////////////
 ## definitions
 
@@ -50,8 +52,42 @@ for (line in lines[-1]) {
     cat("  '",line,"'\n")
     print(num.fields)
   } else {
-    measurements <- rbind(measurements,data.frame(val=meas[1], stat=meas[2], syst=meas[3], bibitem=fields[4])) 
+    measurements =
+      rbind(measurements,data.frame(val=meas[1],
+                                    stat=meas[2],
+                                    syst=meas[3],
+                                    bibitem=fields[4]))
   }
 }
-measurements
 
+meas = measurements$val
+error = sqrt(measurements$stat^2 + measurements$syst^2)
+
+x.min = min(meas-error)
+x.max = max(meas+error)
+x.range = x.max - x.min
+x.min = x.min-0.2*x.range
+x.max = x.max+0.2*x.range
+ratio.labels = 1/2
+x.max.plot = x.min + (x.max-x.min)/(1-ratio.labels)
+
+y.num = length(meas)
+y.tot = y.num+2
+y.min = (y.tot - y.num +1)/2 + 0.5
+y.max = (y.tot + y.num -1)/2 + 0.5
+
+postscript("average.eps")
+line.height = par("cin")[2]
+dev.height = line.height*(par("mar")[1] + par("mar")[3] + y.num+2)
+rc = dev.off()
+
+postscript("average.eps", onefile=FALSE, width=8, height=dev.height,
+           paper="special", horizontal=FALSE, title=meas.label)
+
+plotCI(meas, y.min:y.max, uiw=error, err="x",
+  xlim=c(x.min, x.max.plot), ylim=c(1,y.tot),
+  xlab="", ylab="", yaxt = "n", pch=16, lwd=1.5, cex=0.7,
+  main=meas.label)
+
+text(x.max, y.min:y.max, pos=4, labels=measurements$bibitem, cex=0.9)
+rc = dev.off()
