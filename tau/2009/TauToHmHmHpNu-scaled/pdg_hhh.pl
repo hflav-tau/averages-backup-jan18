a3df3438a9e6f0aabf63edfb45b4a1773749fef9
Swagato.Banerjee <Swagato.Banerjee@0b3a14d8-1776-11df-914e-2594af177612>
2010-04-11 13:44:20 +0000

    update pdg_average.cc

--- tau/2009/TauToHmHmHpNu-scaled/pdg_hhh.pl
+++ tau/2009/TauToHmHmHpNu-scaled/pdg_hhh.pl
@@ -8,6 +8,12 @@ $sfac3 = 1.55071;
 $sfac4 = 5.44599;
 $sfac5 = 1.43062;
 
+$sfac1=2.08777669;
+$sfac2=2.08777669;
+$sfac3=2.08777669;
+$sfac4=2.08777669;
+$sfac5=2.08777669;
+
 printf "* B(tau- -> h- h- h+ nu) [ex K0]\n";
 printf "BEGIN       DELPHI HmHmHpNu published ABDALLAH	 06A\n";
 printf "\n";
