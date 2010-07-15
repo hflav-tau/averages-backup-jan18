f093c5860df54c515cd83881a2029c5e8f0943ae
alusiani <alusiani@0b3a14d8-1776-11df-914e-2594af177612>
2010-07-15 23:53:20 +0000

    update

--- tau/2009/TauFit-alu/aluelab-results.r
+++ tau/2009/TauFit-alu/aluelab-results.r
@@ -22,21 +22,106 @@ load(file.name)
 
 quant.val = quant
 rm(quant)
+quant.err = quant3.err
+quant.corr = quant3.corr
+quant.cov = quant3.cov
 
-stop("")
+##
+## Gamma44 = pi K0 pi0 pi0 nu
+## PDG 2009 ( 2.6 ± 2.4 ) × 10~3 
+##
+aeb.meas.add.single("Gamma44", 2.6e-4, 2.4e-4)
 
-##-- compute B(tau -> hhh nu) as linear combination of averaged quantities
-## meas.sum.lc = meas.val*0 + rep(1, length(meas.val))
-meas.sum = aeb.linear.comb.glob(c(
-  Gamma3=1,   Gamma5=1,   Gamma9=1,   Gamma10=1,  Gamma14=1,  Gamma16=1, 
-  Gamma20=1,  Gamma23=1,  Gamma27=1,  Gamma28=1,  Gamma30=1,  Gamma35=1, 
-  Gamma37=1,  Gamma40=1,  Gamma42=1,  Gamma47=1,  Gamma48=1,  Gamma62=1, 
-  Gamma70=1,  Gamma77=1,  Gamma78=1,  Gamma85=1,  Gamma89=1,  Gamma93=1, 
-  Gamma94=1,  Gamma103=1, Gamma104=1, Gamma126=1, Gamma128=1, Gamma130=1,
-  Gamma132=1, Gamma150=1, Gamma152=1)) 
+##
+## Gamma53 =  K0 h+ h~ h~ nu
+## PDG 2009 ( ( 2.3 ± 2.0 ) × 10~4
+##
+aeb.meas.add.single("Gamma53", 2.3e-4, 2.0e-4)
 
-cat("aluelab-taufit.r: using the following measurements:\n")
-show(rbind(val=meas.val, err=meas.err))
+##
+## Gamma129 = K*(892) eta nu
+## PDG 2009 ( 1.38 ± 0.15 ) × 10~4 
+##
+aeb.meas.add.single("Gamma129", 1.38e-4, 0.15e-4)
 
-cat("aluelab-taufit.r: result of sum of all basic modes:\n")
-show(rbind(sum=meas.sum))
+##
+## Gamma130 = eta K pi0 nu
+## PDG 2009 ( 4.8 ± 1.2 ) × 10 ~ 5
+##
+aeb.meas.add.single("Gamma130", 4.8e-5, 1.2e-5)
+
+##
+## Gamma132 = eta K0 pi nu
+## PDG 2009 ( 9.3 ± 1.5 ) × 10 ~ 5
+##
+aeb.meas.add.single("Gamma132", 9.3e-5, 1.5e-5)
+
+##
+## Gamma144 = K phi nu
+## KmPhiNu HFAG 2009 3.704330e-05 +- 3.259888e-06 (S=1.3)
+##
+aeb.meas.add.single("Gamma144", 3.704330e-05, 3.259888e-06)
+
+##
+## Gamma151 = K omega nu
+## PDG 2009 ( 4.1 ± 0.9 ) × 10 ~ 4
+##
+aeb.meas.add.single("Gamma151", 4.1e-4, 0.9e-4)
+
+##
+## B[tau- -> (K3pi)- nu (ex. K0, omega,eta)]  : 1e-2 : 0.074 ± 0.030    
+##
+aeb.meas.add.single("Km3PiNu", 0.074e-2, 0.030e-2)
+
+##
+## B[tau- -> (K4pi)- nu]                      : 1e-2 : 0.011 ± 0.007
+##
+aeb.meas.add.single("Km4PiNu", 0.011e-2, 0.007e-2)
+
+## S035B33  Gamma40       G(pi- Kbar0 pi0 nu(tau))/G(total)
+## S035C1   Gamma48       G(pi- K(S)0 K(L)0 nu(tau))/G(total)
+
+##
+## Sudan's paper
+## B(tau -> KSKL pi nu) = 0.00087 +- 0.000085 +- 0.000030
+## - va aggiunta alle misure se approvata in tempo
+##
+
+##--- measurement names matching a specific PDG Gamma
+meas.match = function(gamma) {
+  return( regexpr(paste("[.]", "Gamma40", "[.]", sep=""), names(meas)) != -1 )
+}
+
+##--- sum in quadrature
+quadrature = function(x) {
+  return(sqrt(sum(x^2)))
+}
+
+tau.to.s.pdg08 = aeb.linear.comb.glob(c(
+  Gamma10=1,  Gamma16=1, 
+  Gamma23=1,  Gamma28=1,  Gamma35=1, 
+  Gamma40=1,  Gamma85=1,  Gamma89=1,
+  Gamma128=1))
+
+##--- Gamma89  = K pi+pi- pi0 nu
+
+##
+## tau -> s Swagato
+## - Gamma110 = Gamma10 + Gamma16 + Gamma35 + Gamma23 + Gamma40 + Gamma85 + Gamma128 + Gamma129 + Gamma144 + estimates
+## - replace estimates with PDG values
+##
+sel.swb = c(
+  Gamma10=1,  Gamma16=1, 
+  Gamma23=1,  Gamma28=1,  Gamma35=1, 
+  Gamma40=1,  Gamma44=1,  Gamma53=1,
+  Gamma85=1,  Gamma89=1,
+  Gamma128=1, Gamma129=0,
+  Gamma130=1, Gamma132=1,
+  Gamma144=1,
+  Gamma151=1, Km3PiNu=0, Km4PiNu=0)
+tau.to.s.swb = aeb.linear.comb.glob(sel.swb)
+sel.swb.names = names(sel.swb[sel.swb != 0])
+
+show(rbind(cbind(val=quant.val[sel.swb.names], err=quant.err[sel.swb.names]),
+           Gamma110=tau.to.s.swb))
+
