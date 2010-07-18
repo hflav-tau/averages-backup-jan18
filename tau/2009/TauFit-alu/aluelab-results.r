08f30ee485f26a3f4057cf9dddef6292c635e6f8
alusiani <alusiani@0b3a14d8-1776-11df-914e-2594af177612>
2010-07-18 09:17:17 +0000

    copy also average_alucomb.rdata to data directory

--- tau/2009/TauFit-alu/aluelab-results.r
+++ tau/2009/TauFit-alu/aluelab-results.r
@@ -4,9 +4,15 @@
 ##
 ## aluelab-results.r
 ##
-## - elaborate of alucomb.r fit results
+## - elaborate alucomb.r fit results
 ##
 ## ////////////////////////////////////////////////////////////////////////////
+
+##
+## Sudan's paper
+## B(tau -> KSKL pi nu) = 0.00087 +- 0.000085 +- 0.000030
+##+++ include if approved in time
+##
 
 source("../../../Common/bin/aluelab.r")
 
@@ -70,15 +76,6 @@ rm(meas)
 ##
 ##+++ aeb.meas.add.single("Km4PiNu", 0.011e-2, 0.007e-2)
 
-## S035B33  Gamma40       G(pi- Kbar0 pi0 nu(tau))/G(total)
-## S035C1   Gamma48       G(pi- K(S)0 K(L)0 nu(tau))/G(total)
-
-##
-## Sudan's paper
-## B(tau -> KSKL pi nu) = 0.00087 +- 0.000085 +- 0.000030
-##+++ include if approved in time
-##
-
 ##--- measurement names matching a specific PDG Gamma
 meas.match = function(gamma) {
   return( regexpr(paste("[.]", gamma, "[.]", sep=""), names(meas.val)) != -1 )
@@ -89,42 +86,59 @@ quadrature = function(x) {
   return(sqrt(sum(x^2)))
 }
 
-tau.to.s.pdg08 = aeb.linear.comb.glob(c(
+##
+## PDG 2009 definitio of Gamma110 = B(tau -> Xs nu)
+##
+B_tau_s.pdg09.val.err = aeb.linear.comb.glob(c(
   Gamma10=1,  Gamma16=1, 
   Gamma23=1,  Gamma28=1,  Gamma35=1, 
   Gamma40=1,  Gamma85=1,  Gamma89=1,
   Gamma128=1))
 
-##--- Gamma89  = K pi+pi- pi0 nu
-
 ##
-## tau -> s Swagato
-## - Gamma110 = Gamma10 + Gamma16 + Gamma35 + Gamma23 + Gamma40 + Gamma85 + Gamma128 + Gamma129 + Gamma144 + estimates
-## - replace estimates with PDG values
+## compute Gamma110 = B(tau -> Xs)
 ##
-## Gamma110 = Gamma10+Gamma16+Gamma23+Gamma28+Gamma35+Gamma40+Gamma44+Gamma53+Gamma85+Gamma89+Gamma128+Gamma130+Gamma132+Gamma144
+## Gamma110 =
+##   Gamma10+Gamma16+Gamma23+Gamma28+Gamma35+Gamma40+Gamma44+
+##   Gamma53+Gamma85+Gamma89+Gamma128+Gamma130+Gamma132+
+##   Gamma96+Gamma96*B(phi -> KS0 KL0)/B(phi -> K+ K-)
 ##
-sel.swb = c(
+B_tau_s.sel = c(
   Gamma10=1,  Gamma16=1, 
   Gamma23=1,  Gamma28=1,  Gamma35=1, 
   Gamma40=1,  Gamma44=1,  Gamma53=1,
-  Gamma85=1,  Gamma89=1,
-  Gamma128=1,
+  Gamma85=1,  Gamma89=1,  Gamma128=1,
   Gamma130=1, Gamma132=1,
   Gamma96=1.699387)
-tau.to.s.swb = aeb.linear.comb.glob(sel.swb)
-sel.swb.names = names(sel.swb[sel.swb != 0])
+B_tau_s.val.err = aeb.linear.comb.glob(B_tau_s.sel)
+B_tau_s.names = names(B_tau_s.sel[B_tau_s.sel != 0])
+B_tau_s.val = B_tau_s.val.err["val"]
+B_tau_s.err = B_tau_s.val.err["err"]
 
 ##
-## universality B(tau -> e nu nubar)
+## B(tau -> Xs nu)
+##
+## use Gamma110 via COMBOFQUANT in alucomb.r
+## same definition as above
+##
+B_tau_s.val = quant.val["Gamma110"]
+B_tau_s.err = quant.err["Gamma110"]
+
+##
+## universality Be = B(tau -> e nu nubar)
+##
+## minimum chisq fit constraining Bmu/Be from Standard Model,
+## using Be, Bmu and their covariance
+##
+
 ##
 ## Bmu/Be = f(m_mu^2/m_tau^2) / f(m_e^2/m_tau^2) = 0.972565 +- 0.000009 -- PDG 2004
 ## http://pi.physik.uni-bonn.de/~brock/teaching/vtp_ss06/doc/davier_0507078.pdf
 ##
-B.tau.mu.by.e.th = 0.972565
+B.tau.mu.by.e.th.val = 0.972565
 B.tau.mu.by.e.th.err = 0.000009
 
-B_tau_e_univ.sel = c(Gamma5=1, Gamma3=B.tau.mu.by.e.th)
+B_tau_e_univ.sel = c(Gamma5=1, Gamma3=B.tau.mu.by.e.th.val)
 B_tau_e_univ.names = names(B_tau_e_univ.sel)
 
 quant.mu.e.val = quant.val[B_tau_e_univ.names]
@@ -135,13 +149,17 @@ quant.mu.e.invcov = solve(quant.mu.e.cov)
 B_tau_e_univ.cov = solve(t(B_tau_e_univ.delta) %*% solve(quant.mu.e.cov) %*% B_tau_e_univ.delta)
 quant.mu.e.comb = B_tau_e_univ.cov %*% t(B_tau_e_univ.delta) %*% quant.mu.e.invcov
 names(quant.mu.e.comb) = names(quant.mu.e.val)
+
 B_tau_e_univ.val = quant.mu.e.comb %*% quant.mu.e.val
 B_tau_e_univ.err = sqrt(diag(B_tau_e_univ.cov))
 
 B_tau_e_univ.comb = quant.val * 0
 B_tau_e_univ.comb[names(quant.mu.e.comb)] = quant.mu.e.comb
 
-##--- add quantity that is combination of existing quantities
+##
+## add quantity that is combination of existing quantities
+## by updating the quantities vector and covariance matrix
+##
 quant.val = c(quant.val, Gamma5univ = B_tau_e_univ.val)
 quant.cov = rbind(
   cbind(quant.cov,
@@ -149,33 +167,34 @@ quant.cov = rbind(
   Gamma5univ=c(B_tau_e_univ.comb %*% quant.cov, B_tau_e_univ.err^2))
 quant.corr = quant.cov / sqrt(diag(quant.cov)) %o% sqrt(diag(quant.cov))
 
-##--- B(tau -> Xs nu)
-B_tau_s.val = tau.to.s.swb["val"]
-B_tau_s.err = tau.to.s.swb["err"]
-B_tau_s.val = quant.val["Gamma110"]
-B_tau_s.err = quant.err["Gamma110"]
-
-##--- Vud
+##
+## Vud
+##
+## arXiv:0710.3181v1 [nucl-th], 10.1103/PhysRevC.77.025501
+## I.S.Towner, J.C.Hardy, An improved calculation of the isospin-symmetry-breaking corrections to superallowed Fermi beta decay
+##
 Vud.val = 0.97425
 Vud.err = 0.00022
 
-##--- SU3 breaking correction 0.240 +- 0.032
+##
+## SU3 breaking correction 
+##
 DeltaR.su3viol.val = 0.240
 DeltaR.su3viol.err = 0.032
 
-vus.fun = function(B_tau_s.val, B_tau_e_univ.val, Vud.val, DeltaR.su3viol.val) {
-  Rtau <<- 1/B_tau_e_univ.val -1 -B.tau.mu.by.e.th
+Vus.fun = function(B_tau_s.val, B_tau_e_univ.val, Vud.val, DeltaR.su3viol.val) {
+  Rtau <<- 1/B_tau_e_univ.val -1 -B.tau.mu.by.e.th.val
   Rtau.s <<- B_tau_s.val / B_tau_e_univ.val
   Rtau.ns <<- Rtau - Rtau.s
   return(sqrt(Rtau.s/(Rtau.ns/Vud.val^2 - DeltaR.su3viol.val)))
 }
 
-Vus.val = vus.fun(B_tau_s.val, B_tau_e_univ.val, Vud.val, DeltaR.su3viol.val)
-
+Vus.val = Vus.fun(B_tau_s.val, B_tau_e_univ.val, Vud.val, DeltaR.su3viol.val)
 Vus.err = 0
 
-show(rbind(cbind(val=quant.val[sel.swb.names], err=quant.err[sel.swb.names]),
-           Gamma110 = tau.to.s.swb,
+show(rbind(cbind(val=quant.val[B_tau_s.names], err=quant.err[B_tau_s.names]),
+           Gamma110 = B_tau_s.val.err,
+           Gamma110_pdg09 = B_tau_s.pdg09.val.err,
            B_tau_e_univ = data.frame(val=B_tau_e_univ.val, err=B_tau_e_univ.err),
            R_tau_h = data.frame(val=Rtau, err=0),
            R_tau_S = data.frame(val=Rtau.s, err=0),
