a1e19eb55e3faf439189a0c455bdf018e50bd78f
alusiani <alusiani@0b3a14d8-1776-11df-914e-2594af177612>
2010-07-20 15:05:12 +0000

    add R script to list results of alucomb.r

---
+++ Common/bin/aluelab-list.r
@@ -0,0 +1,35 @@
+#!/usr/bin/env Rscript
+
+## ////////////////////////////////////////////////////////////////////////////
+##
+## aluelab-list.r
+##
+## - elaborate alucomb.r fit results
+##
+## ////////////////////////////////////////////////////////////////////////////
+
+require(methods)
+
+args <- commandArgs(TRUE)
+if (length(args) > 0) {
+  file.name = args[1]
+} else {
+  file.name = "average_alucomb.rdata"
+}
+
+##--- get alucomb results and data
+load(file.name)
+
+quant.val = quant
+rm(quant)
+
+##--- list a subtset of fitted quantities
+display.names = c(
+  "Gamma3", "Gamma5", "Gamma110"
+  )
+
+##--- list all fitted quantities
+display.names = names(quant.val)
+
+show(rbind(cbind(val=quant.val[display.names], err=quant.err[display.names])))
+
