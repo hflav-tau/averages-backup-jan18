f72985c8093e9da3dc84d96765eff8335251ed2c
alusiani <alusiani@0b3a14d8-1776-11df-914e-2594af177612>
2010-08-20 21:11:39 +0000

    update

--- tau/2009/TauFit-pdg-alu/compare_alucomb_pdg.r
+++ tau/2009/TauFit-pdg-alu/compare_alucomb_pdg.r
@@ -5,7 +5,7 @@
 ## compare_alucomb_pdg.r
 ##
 ## - get averages from alucomb.r .rdata file
-## - get PDG averages from pdginput/pdgread.cc
+## - get PDG averages from pdginput/pdgread.cc and pdginput/s035-fit-no-babar-belle.datx
 ##
 ## ////////////////////////////////////////////////////////////////////////////
 
@@ -54,7 +54,7 @@ ll =
 con = textConnection(ll)
 pdg = read.table(con, sep=";", col.names=labels)
 close(con)
-pdg$quant.name = paste("Gamma", pdg$gamma, sep="")
+pdg$quant.names = paste("Gamma", pdg$gamma, sep="")
 
 ##--- get data from Swagato .cc file
 ll = readLines(file.name.pdg.swb)
@@ -73,91 +73,141 @@ ll = gsub(paste("^.*basegamma.push_back.\\s*(\\S+)\\s*.;",
 
 con = textConnection(ll)
 pdg.swb = read.table(con, sep=";",
-  col.names = c("quant.id", "quant.seed", "quant", "quant.err", "quant3.err", "sfact3.types"))
+  col.names = c("quant.id", "quant.seed", "quant.val", "quant.err", "quant.sf.err", "quant.sf.sfact"))
 close(con)
-pdg.swb$quant.name = paste("Gamma", pdg.swb$quant.id, sep="")
+pdg.swb$quant.names = paste("Gamma", pdg.swb$quant.id, sep="")
 
-quant.names = names(quant)
+quant.names = names(quant.val)
 
-pdg.select = quant.names %in% pdg.swb$quant.name
+pdg.select = quant.names %in% pdg.swb$quant.names
 quant.names.pdg = quant.names[pdg.select]
-pdg.swb.sel = match(quant.names.pdg, pdg.swb$quant.name)
+pdg.swb.sel = match(quant.names.pdg, pdg.swb$quant.names)
 
-## pdg.rows = sapply(unique(pdg$quant.name), function(x) which(x == pdg$quant.name)[1])
-pdg.rows = match(quant.names.pdg, pdg$quant.name)
+## pdg.rows = sapply(unique(pdg$quant.names), function(x) which(x == pdg$quant.names)[1])
+pdg.rows = match(quant.names.pdg, pdg$quant.names)
 pdg.rows = pdg.rows[!is.na(pdg.rows)]
 
-cat("             quant     err       err3      sf3  pdg       pdg.err   pdg.err3  pdg.sf3 Dquant%    Derr%       Derr2%    Dsf% \n")
+##
+## compare results pdg vs. alucomb
+##
+compare.1 = function() {
+
+cat("           quant     err       err.sf    sf   pdg       pdg.err   pdg.err.sf pdg.sf  Dquant%    Derr%      Derr.sf%   Dsf%\n")
 rc = mapply(function(
   name,
-  quant,
+  quant.val,
   quant.err,
-  quant3.err,
-  sfact3.types,
-  pdg.quant,
+  quant.sf.err,
+  quant.sf.sfact,
+  pdg.quant.val,
   pdg.quant.err,
-  pdg.quant3.err,
-  pdg.sfact3.types
+  pdg.quant.sf.err,
+  pdg.quant.sf.sfact
   ) {
-  cat(sprintf("%12s %9.7f %9.7f %9.7f %4.2f %9.7f %9.7f %9.7f %4.2f %9.4f%% %9.4f%% %9.4f%% %9.4f%%\n",
+  cat(sprintf("%10s %9.7f %9.7f %9.7f %4.2f %9.7f %9.7f %9.7f %5.2f %9.4f%% %9.4f%% %9.4f%% %9.4f%%\n",
               name,
-              quant,
+              quant.val,
               quant.err,
-              quant3.err,
-              sfact3.types,
-              pdg.quant,
+              quant.sf.err,
+              quant.sf.sfact,
+              pdg.quant.val,
               pdg.quant.err,
-              pdg.quant3.err,
-              pdg.sfact3.types,
-              (quant/pdg.quant-1)*100,
+              pdg.quant.sf.err,
+              pdg.quant.sf.sfact,
+              (quant.val/pdg.quant.val-1)*100,
               (quant.err/pdg.quant.err-1)*100,
-              (quant3.err/pdg.quant3.err-1)*100,
-              (sfact3.types/pdg.sfact3.types-1)*100
+              (quant.sf.err/pdg.quant.sf.err-1)*100,
+              (quant.sf.sfact/pdg.quant.sf.sfact-1)*100
               ))
 },
   quant.names.pdg,
-  quant[quant.names.pdg],
+  quant.val[quant.names.pdg],
   quant.err[quant.names.pdg],
-  quant3.err[quant.names.pdg],
-  sfact3.types[quant.names.pdg],
-  ## pdg.quant$fit.value[pdg.rows],
-  ## pdg.quant$fit.error,
-  ## pdg.quant$fit.scalederr,
-  ## pdg.quant$scale
-  pdg.swb$quant[pdg.swb.sel],
+  quant.sf.err[quant.names.pdg],
+  quant.sf.sfact[quant.names.pdg],
+  pdg.swb$quant.val[pdg.swb.sel],
   pdg.swb$quant.err[pdg.swb.sel],
-  pdg.swb$quant3.err[pdg.swb.sel],
-  pdg.swb$sfact3.types[pdg.swb.sel]
+  pdg.swb$quant.sf.err[pdg.swb.sel],
+  pdg.swb$quant.sf.sfact[pdg.swb.sel]
   )
+}
+
+##
+## compare results pdg vs. alucomb
+##
+compare.2 = function() {
+
+cat("            pdg   or.sc or.fq al.sc al.fq or.fu al.fu\n")
+rc = mapply(function(
+  name,
+  pdg.quant.sf.sfact,
+  orin.sc.quant.sfact,
+  orin.fq.quant.sfact,
+  alu.sc.quant.sfact,
+  alu.fq.quant.sfact,
+  orin.full.quant.sfact,
+  alu.full.quant.sfact
+  ) {
+  cat(sprintf("%10s %5.2f %5.2f %5.2f %5.2f %5.2f %5.2f %5.2f\n",
+              name,
+              pdg.quant.sf.sfact,
+              orin.sc.quant.sfact,
+              orin.fq.quant.sfact,
+              alu.sc.quant.sfact,
+              alu.fq.quant.sfact,
+              orin.full.quant.sfact,
+              alu.full.quant.sfact
+              ## (quant.val/pdg.quant.val-1)*100,
+              ## (quant.err/pdg.quant.err-1)*100,
+              ## (quant.sf.err/pdg.quant.sf.err-1)*100,
+              ## (quant.sf.sfact/pdg.quant.sf.sfact-1)*100
+              ))
+},
+  quant.names.pdg,
+  pdg.swb$quant.sf.sfact[pdg.swb.sel],
+  orin.sc$quant.sfact[quant.names.pdg],
+  orin.fq$quant.sfact[quant.names.pdg],
+  alu.sc$quant.sfact[quant.names.pdg],
+  alu.fq$quant.sfact[quant.names.pdg],
+  orin.full$quant.sfact[quant.names.pdg],
+  alu.full$quant.sfact[quant.names.pdg]
+  )
+}
+
+compare.2()
+
+##
+## obsolete code follows
+##
 
 if (FALSE) {
 
-show(rbind(fit       = quant[quant.names.pdg],
-           pdg       = pdg.quant[quant.names.pdg],
+show(rbind(fit       = quant.val[quant.names.pdg],
+           pdg       = pdg.quant.val[quant.names.pdg],
            fit.err   = quant.err[quant.names.pdg],
            pdg.err   = pdg.quant.err[quant.names.pdg],
            fit.err2  = quant2.err[quant.names.pdg],
            pdg.err2  = pdg.quant2.err[quant.names.pdg],
-           fit.sf    = sfact3.types[quant.names.pdg],
-           pdg.sf    = pdg.sfact3.types[quant.names.pdg],
-           "Dquant%" = (quant[quant.names.pdg]/pdg.quant[quant.names.pdg]-1)*100,
+           fit.sf    = quant.sf.sfact[quant.names.pdg],
+           pdg.sf    = pdg.quant.sf.sfact[quant.names.pdg],
+           "Dquant%" = (quant[quant.names.pdg]/pdg.quant.val[quant.names.pdg]-1)*100,
            "Derr%"   = (quant.err[quant.names.pdg]/pdg.quant.err[quant.names.pdg]-1)*100,
            "Derr2%"  = (quant2.err[quant.names.pdg]/pdg.quant2.err[quant.names.pdg]-1)*100,
-           "Dsf2%"   = (sfact3.types[quant.names.pdg]/pdg.sfact3.types[quant.names.pdg]-1)*100
+           "Dsf2%"   = (quant.sf.sfact[quant.names.pdg]/pdg.quant.sf.sfact[quant.names.pdg]-1)*100
            ))
 
 show(rbind(fit       = quant[quant.names.pdg],
-           pdg       = pdg.quant[quant.names.pdg],
+           pdg       = pdg.quant.val[quant.names.pdg],
            fit.err   = quant.err[quant.names.pdg],
            pdg.err   = pdg.quant.err[quant.names.pdg],
            fit.err2  = quant2.err[quant.names.pdg],
            pdg.err2  = pdg.quant2.err[quant.names.pdg],
-           fit.sf    = sfact3.types[quant.names.pdg],
-           pdg.sf    = pdg.sfact3.types[quant.names.pdg],
-           "Dquant%" = sprintf("%7.5f", (quant[quant.names.pdg]/pdg.quant[quant.names.pdg]-1)*100),
+           fit.sf    = quant.sf.sfact[quant.names.pdg],
+           pdg.sf    = pdg.quant.sf.sfact[quant.names.pdg],
+           "Dquant%" = sprintf("%7.5f", (quant.val[quant.names.pdg]/pdg.quant.val[quant.names.pdg]-1)*100),
            "Derr%"   = sprintf("%7.5f", (quant.err[quant.names.pdg]/pdg.quant.err[quant.names.pdg]-1)*100),
            "Derr2%"  = sprintf("%7.5f", (quant2.err[quant.names.pdg]/pdg.quant2.err[quant.names.pdg]-1)*100),
-           "Dsf2%"   = sprintf("%7.5f", (sfact3.types[quant.names.pdg]/pdg.sfact3.types[quant.names.pdg]-1)*100)
+           "Dsf2%"   = sprintf("%7.5f", (quant.sf.sfact[quant.names.pdg]/pdg.quant.sf.sfact[quant.names.pdg]-1)*100)
            ))
 
 ##---
@@ -172,19 +222,19 @@ options.save = options()
 options(width=132)
 
 rc = print.wo.rownames(data.frame(
-  Gamma=as.character(pdg$quant.name),
+  Gamma=as.character(pdg$quant.names),
   Descr=as.character(pdg$descr)))
 
 options(options.save)
 
 ##---
 
-pdg.rows = sapply(unique(pdg$quant.name[order(pdg$quant.name)]), function(x) which(x == pdg$quant.name)[1])
-quant.name.maxwidth = max(nchar(pdg$quant.name[pdg.rows]))
+pdg.rows = sapply(unique(pdg$quant.names[order(pdg$quant.names)]), function(x) which(x == pdg$quant.names)[1])
+quant.name.maxwidth = max(nchar(pdg$quant.names[pdg.rows]))
 node.maxwidth = max(nchar(as.character(pdg$node[pdg.rows])))
 rc = mapply(function(node, quant.name, descr) {
   cat(sprintf(paste("%-", node.maxwidth+1, "s %-", quant.name.maxwidth+1, "s %s\n", sep=""), node, quant.name, descr))
-}, pdg$node[pdg.rows], pdg$quant.name[pdg.rows], pdg$descr[pdg.rows])
+}, pdg$node[pdg.rows], pdg$quant.names[pdg.rows], pdg$descr[pdg.rows])
 
 }
 
