1b91f67ffc41acd2cfad761f10759d79063220e1
alusiani <alusiani@0b3a14d8-1776-11df-914e-2594af177612>
2011-08-18 07:31:08 +0000

    utilities to compare results

--- Common/bin/alucomb2.sh
+++ Common/bin/alucomb2.sh
@@ -2,7 +2,7 @@
 # -*- ksh -*-
 
 #
-# alucomb.sh
+# alucomb2.sh
 #
 
 usage ()
@@ -24,7 +24,7 @@ if [ "$file" = "" ] ; then
   usage
 fi
 
-/bin/rm -f average.input average_alucomb.rdata average.log
+/bin/rm -f average.input average.rdata average.log
 
 if [ ! -r $file -o -z $file ] ; then
   echo "file '$file' must exists and be non-empty"
@@ -35,13 +35,13 @@ name=`expr "$file" : '\(.*\)\..*'`
 
 /bin/ln -s $file average.input
 
-echo 1>&2 "make alucomb"
-make alucomb
+echo 1>&2 "make alucomb2"
+make alucomb2
 if [ $? != 0 ] ; then
   exit $?
 fi
 
-/bin/cp -p average_alucomb.rdata ${name}.rdata
+/bin/cp -p average.rdata ${name}.rdata
 /bin/cp -p average.log ${name}.log
 
 echo 1>&2 "produced file: ${name}.rdata"
---
+++ Common/bin/aluelab-results2.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+# -*- ksh -*-
+
+#
+# aluelab-results2.sh
+#
+# execute alucomb2.r and print aluelab-results.r results
+#
+
+usage ()
+{
+  echo 1>&2 "usage: `basename $0`"
+  exit 1
+}
+
+while getopts h opt; do
+  case "$opt" in
+    h) usage ;;
+    *) usage ;;
+  esac
+done
+shift `expr $OPTIND - 1`
+
+file="$1"
+if [ "$file" = "" ] ; then
+  file="average.input"
+fi
+
+name=`expr "$file" : '\(.*\)\..*'`
+
+../../../Common/bin/alucomb2.sh $file
+if [ $? != 0 ] ; then
+  exit 1
+fi
+fileres="${name}-results.log"
+echo 1>&2 "producing file: ${fileres}"
+/bin/rm -f ${fileres} ; ./aluelab-results.r "${name}.rdata" | tee ${fileres}
+
+exit
