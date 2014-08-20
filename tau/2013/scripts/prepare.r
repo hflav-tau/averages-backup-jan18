7ebd13de648c0ca93d6ee5422287616548ccc5d8
alusiani <alusiani@0b3a14d8-1776-11df-914e-2594af177612>
2014-08-20 14:58:51 +0000

    added extra common syst. contr. to Belle KS paper results

--- tau/2013/scripts/prepare.r
+++ tau/2013/scripts/prepare.r
@@ -16,6 +16,8 @@ rc = quant$meas.add.single("Be", 0.1783, 0.0004)
 rc = quant$meas.add.single("Bmu", 0.1741, 0.0004)
 
 ##
+## Belle KS paper results
+##
 ## some BR depend on e-mu normalization
 ## and have syst. contr. from Be and Bmu
 ## the BR expression has the following factor
@@ -28,12 +30,37 @@ e_mu_norm.rel.syst.perc =  quant$err()["e_mu_norm"] / quant$val()["e_mu_norm"] *
 e_mu_norm.rel.syst.Be.perc = quant$syst.contrib.perc("e_mu_norm", "Be")
 e_mu_norm.rel.syst.Bmu.perc = quant$syst.contrib.perc("e_mu_norm", "Bmu")
 
+##
+## Belle KS paper results
+##
+## correlated systematic contribution due to e-mu normalization
+##
+
+##--- from the paper
+e_mu_norm.re.syst.total.perc = 0.5
+
+##--- part of e-mu normalization syst. contribution not due to leptonic BRs
+e_mu_norm.re.syst.nonBl.perc = sqrt(e_mu_norm.re.syst.total.perc^2 - e_mu_norm.rel.syst.perc^2)
+
+##
+## print
+##
+
+cat("#\n")
+cat("# Belle paper on KS BRs\n")
+cat("#\n")
+
 cat(sprintf("# total systematic contr. for e-mu normalization: %.4g%%\n", e_mu_norm.rel.syst.perc))
 cat("SYSTEMATICS\n")
 cat(sprintf(" %8.4g%%         EmNuNumb         # e-mu normalization, Be contr.\n",  e_mu_norm.rel.syst.Be.perc));
 cat(sprintf(" %8.4g%%         MumNuNumb        # e-mu normalization, Bmu contr.\n",  e_mu_norm.rel.syst.Bmu.perc));
 cat("\n")
 
+cat("SYSTPAPER\n")
+cat(sprintf(" %8.4g%%         e_mu_norm        # e-mu normalization, non Bl contr.\n",  e_mu_norm.re.syst.nonBl.perc));
+cat("\n")
+
 cat("PARAMETERS\n")
 cat(sprintf("  EmNuNumb        %8.4g          +-%.4g\n",  quant$val("Be"), quant$err("Be")));
 cat(sprintf("  MumNuNumb       %8.4g          +-%.4g\n",  quant$val("Bmu"), quant$err("Bmu")));
+
