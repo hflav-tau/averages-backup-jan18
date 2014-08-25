44cd01a72494454a0855a75d801ec76329c71698
alusiani <alusiani@0b3a14d8-1776-11df-914e-2594af177612>
2014-08-25 17:21:41 +0000

    rename R utils file and functions

--- Common/bin/alureport.r
+++ Common/bin/alureport.r
@@ -7,7 +7,7 @@
 ## return numeric id for sorting labels like "Gamma5", "Gamma3by5"
 ## <n>by<m> are sorted after <n> in ascending order ov <m>
 ##
-alucomb2.gamma.num.id = function(gamma.name) {
+alurep.gamma.num.id = function(gamma.name) {
   gamma.name = sub("Unitarity", "Gamma1000", gamma.name, fixed=TRUE)
   gamma.name = sub("GammaAll", "Gamma999", gamma.name, fixed=TRUE)
   num1 = as.numeric(str_match(gamma.name, ("^(\\D+)(\\d+[.]?\\d*)"))[,3])
