c913de1fe36c50a320b4c42886e40f049af3567f
alusiani <alusiani@0b3a14d8-1776-11df-914e-2594af177612>
2014-08-30 12:49:44 +0000

    print additional faved data from fit

---
+++ tau/2013/scripts/aluelab3-dump.r
@@ -0,0 +1,35 @@
+#!/usr/bin/env Rscript
+
+##
+## aluelab-dump.r [flags] [<.rdata file>]
+## dump some saved info from the fit
+##
+
+## ////////////////////////////////////////////////////////////////////////////
+## functions
+
+## ////////////////////////////////////////////////////////////////////////////
+## code
+
+aluelab.dump = function(args) {
+  if (length(args) > 0) {
+    file.name = args[1]
+  } else {
+    file.name = "average.rdata"
+  }
+
+  ##--- get alucomb results and data
+  load(file.name)
+
+  ## print(quant.corr)
+
+  corr.flag = abs(quant.corr) > 1.0001
+  gamma.flag = apply(corr.flag, 2, function(x) {sum(x)}) != 0
+  gamma.flag = gamma.flag[gamma.flag]
+  gamma.flag.names = names(gamma.flag)
+
+  print(quant.corr[gamma.flag.names, gamma.flag.names])
+}
+
+args = commandArgs(TRUE)
+aluelab.dump(args)
