18563bcf8965c5066dcb154fd6c9efbb94a6acb5
alusiani <alusiani@0b3a14d8-1776-11df-914e-2594af177612>
2014-10-23 08:04:59 +0000

    updated with newer and larger HFAG label

--- tau/2013/TauLFV/TauLFV_UL_plot_comb.cc
+++ tau/2013/TauLFV/TauLFV_UL_plot_comb.cc
@@ -316,7 +316,7 @@ void SetUp()
   gStyle->SetGridStyle(1);
 }
 
-void TauLFV_UL_plot_comb(Int_t when=20141001)
+void TauLFV_UL_plot_comb(Int_t when=2014001)
 {
   SetUp();
   unsigned int ibin;
@@ -438,7 +438,7 @@ void TauLFV_UL_plot_comb(Int_t when=20141001)
   //
   // HFAGTauLabel("Winter 2012", -0.01, -0.02, 1.3);
   // HFAGTauLabel("Summer 2013", -0.01, -0.02, 1.3);
-  HFAGTauLabel("Summer 2014", -0.01, -0.02, 1.3);
+  HFAGTauLabel("Summer 2014", -0.01, -0.02, 1.7);
   c1->Update();  
   
   c1->SaveAs(Form("TauLFV_UL_%d_averaged.pdf",when));
