442d1446a39f4256a35a4dc4fd02ecb0f2d63500
Alberto Lusiani <alusiani@gmail.com>
2016-07-10 02:37:17 +0200

    produce macros from .yaml data file for HFAG 2016

--- tau/2013/TauLFV/scripts/tau-lfv.r
+++ tau/2013/TauLFV/scripts/tau-lfv.r
@@ -18,6 +18,15 @@ require(stringr, quietly=TRUE)
 ##
 ## /////////////////////////////////////////////////////////////////////////////
 
+##--- convert list to data.frame
+list.to.df = function(lst, keyname=NA, stringsAsFactors=FALSE) {
+  rc = do.call(rbind, lapply(lst, function(x) as.data.frame(x, stringsAsFactors=stringsAsFactors)))
+  if (!is.na(keyname)) {
+    rc[[keyname]] = as.character(names(lst))
+  }
+  rc
+}
+
 ##--- convert data.frame to list
 df.to.list = function(df, keyfield=NA, keyname="key") {
   if (!is.na(keyfield)) {
@@ -47,39 +56,24 @@ alurep.tex.cmd.short = Vectorize(alurep.tex.cmd.short.nv)
 ##
 ## /////////////////////////////////////////////////////////////////////////////
 
-##
-## memo on how the data was prepared
-##
-## labels.df = read.csv("a.txt", stringsAsFactors=FALSE)
-## lfv.df = read.delim(ifname, sep="", stringsAsFactors=FALSE)
-## 
-## data.lst = list(gamma.labels=as.list(gamma.labels), lfv.info.df=lfv.df)
-## 
-## ao = as.yaml(data.lst, column.major = FALSE)
-## cat(ao, file="a.yaml")
-##
+tau.lfv.data = yaml.load_file("tau-lfv-data.yaml")
+ofname = "tau-lfv-data.tex"
 
-fname = "tau-lfv-data"
-ifname = paste0(fname, ".yaml")
-ifname2 = paste0(fname, "-comb.yaml")
-ofname = paste(fname, "tex", sep=".")
-
-lfv.data = yaml.load_file(ifname)
-lfv.data.info = yaml.load_file(ifname2)
+gamma.df = list.to.df(tau.lfv.data$gamma)
+names(tau.lfv.data$gamma) = gamma.df$gamma
 
 out.txt = character(0)
 
-rc = mapply(function(gamma.num, label) {
-  label = paste0("g", gamma.num)
-  alurep.tex.cmd.short(paste0(label, ".texlabel"), label)
-}, names(lfv.data.info$br.descr), lfv.data.info$br.descr)
+rc = mapply(function(gamma, descr) {
+  key = paste0("g", gamma)
+  alurep.tex.cmd.short(paste0(key, ".descr"), paste0("\\ensuremath{", descr, "}"))
+}, gamma.df$gamma, gamma.df$descr)
 out.txt = c(out.txt, rc)
 
-rc = lapply(lfv.data.info$comb.info, function(x) {
+if (FALSE) {
+rc = lapply(tau.lfv.data$combs.extra, function(x) {
   rc = character(0)
-  gamma.num = x$gamma
-  exp = tolower(as.vector(str_match(x$result, "([^_]*)_"))[2])
-  label = paste("g", gamma.num, ".", exp, sep="")
+  label = paste0("g", x$gamma, ".", x$ref)
   rc = c(rc, alurep.tex.cmd.short(paste(label, ".lumi", sep=""), x$lumi))
   rc = c(rc, alurep.tex.cmd.short(paste(label, ".xsec", sep=""), x$cross.section))
   rc = c(rc, alurep.tex.cmd.short(paste(label, ".eff", sep=""),
@@ -90,11 +84,68 @@ rc = lapply(lfv.data.info$comb.info, function(x) {
 
   row = paste("\\htuse{", label, ".", c("lumi", "xsec", "eff", "bkg", "evs"), "}", sep="")
   rc = c(rc, alurep.tex.cmd.short(paste(label, ".row", sep=""), paste(row, collapse=" & ")))
-
   rc = paste(rc, collapse="\n")
   rc
 })
 out.txt = c(out.txt, rc)
+}
 
-cat(paste(out.txt, collapse="\n"), "\n", file=ofname)
+tau.lfv.data$combs.extra = tau.lfv.data$combs.extra[
+  order(sapply(tau.lfv.data$combs.extra, function(el) el$gamma))]
+
+rc = lapply(tau.lfv.data$combs.extra, function(br) {
+  gamma.info = tau.lfv.data$gamma[[as.character(br$gamma)]]
+  rc = paste0(
+    "\\htCombExtraLine%\n",
+    "  {\\ensuremath{\\Gamma_{", br$gamma, "} = ", gamma.info$descr, "}}%\n",
+    "  {", br$exp, "}%\n",
+    "  {", br$ref, "}%\n",
+    "  {", br$lumi, "}%\n",
+    "  {", br$cross.section, "}%\n",
+    "  {", sprintf("\\ensuremath{%.2f \\pm %.2f}", br$efficiency*100, br$efficiency.error*100), "}%\n",
+    "  {", sprintf("\\ensuremath{%.2f \\pm %.2f}", br$bkg, br$bkg.error), "}%\n",
+    "  {", br$observed.events, "}"
+  )
+})
+
+rc = alurep.tex.cmd(
+  "CombExtraLines",
+  paste0(rc, collapse="%\n")
+)
+out.txt = c(out.txt, rc)
+
+tau.lfv.data$combs = tau.lfv.data$combs[
+  order(sapply(tau.lfv.data$combs, function(el) el$gamma))]
+
+descr.last = ""
+type.last = ""
+rc = lapply(tau.lfv.data$combs, function(br) {
+  gamma.info = tau.lfv.data$gamma[[as.character(br$gamma)]]
+  rc = character(0)
+  descr = ifelse(gamma.info$descr != descr.last, gamma.info$descr, "")
+  descr.last <<- gamma.info$descr
+  type = ifelse(gamma.info$type != type.last, gamma.info$type, "")
+  type.last <<- gamma.info$type
+
+  if (type != "") {
+    rc = c(rc, "\\hline")
+  }
+  
+  rc = c(
+    rc,
+    paste0(
+      "\\htCombLimitLine%\n",
+      "  {\\ensuremath{\\Gamma_{", br$gamma, "} = ", gamma.info$descr, "}}%\n",
+      "  {\\ensuremath{", type, "}}%\n",
+      "  {\\ensuremath{", sub("^(.*)e([+-]*)0*(\\d*)$", "\\1 \\\\cdot 10^{\\2\\3}", sprintf("%.1e", br$limit)), "}}"
+    ))
+})
+
+rc = alurep.tex.cmd(
+  "CombLines",
+  paste0(unlist(rc), collapse="%\n")
+)
+out.txt = c(out.txt, rc)
+
+cat(paste(out.txt, collapse="\n"), "\n", file=ofname, sep="")
 cat("file '", ofname, "' created\n", sep="")
