8c584843cba54f66309041fa3f293ae8f3275685
Alberto Lusiani <alusiani@gmail.com>
2017-02-19 18:41:08 +0100

    smaller labels

--- tau/2013/plots/alucomb2-plot-leptuniv-sm.r
+++ tau/2013/plots/alucomb2-plot-leptuniv-sm.r
@@ -41,36 +41,42 @@ save.plot = function(name, width=my.graph.width, height=my.graph.height, horiz.p
 ##
 ## HFAG label
 ##
-hfag.label = function(title="HFAG-Tau", subtitle="Summer 2016", scale=0.9, fsratio=0.78, x=unit(0.82,"npc"), y = unit(0.15,"npc")) {
-  ##--- draw title
+hfag.label = function(title="HFAG-Tau", subtitle="Summer 2016", scale=1.2, fsratio=0.78, x=unit(0.845,"npc"), y = unit(0.125,"npc")) {
+  ##
+  ## draw title at desired position, font size scaled
+  ##
   hl.title = textGrob(
     title,
     x = x,
     y = y,
     gp = gpar(fontface="bold.italic", col="white", cex=scale)
-  )  
-  ##--- draw subtitle 
+  )
+  ##
+  ## draw subtitle below title, with smaller font than title by fsratio
+  ##
   hl.subtitle = textGrob(
     subtitle,
     x = hl.title$x,
-    y = hl.title$y - unit(1.4/(fsratio*scale), "grobheight", hl.title),
+    y = hl.title$y - unit(1.3/(fsratio*scale), "grobheight", hl.title),
     gp = gpar(fontface="bold.italic", col="black", cex=fsratio*scale)
   )
-  ##--- draw black title background
+  ##
+  ## draw black title background
+  ##
   hl.title.bkg = rectGrob(
     x = hl.title$x,
     y = hl.title$y,
-    width = unit(1/scale, "grobwidth", hl.title) + 0.6*unit(1/scale, "grobheight", hl.title),
-    height = unit(1/scale, "grobheight", hl.title) + 0.6*unit(1/scale, "grobheight", hl.title),
-    vjust = 0.5,
+    width = unit(1/scale, "grobwidth", hl.title) + unit(0.02, "npc"),
+    height = unit(1, "grobheight", hl.title) + unit(0.02, "npc"),
     gp=gpar(fill="black", col="black", lwd=1, cex=scale)
   )
-  ##--- draw subtitle background  
+  ##--- draw subtitle background
   hl.box = rectGrob(
     x = hl.subtitle$x,
     y = hl.subtitle$y,
-    width = unit(1/(fsratio*scale), "grobwidth", hl.title.bkg),
-    height = unit(1, "grobheight", hl.subtitle) + 0.8*unit(1/(fsratio), "grobheight", hl.title),
+    width = unit(1/(fsratio*scale), "grobwidth", hl.title.bkg) - unit(0.003, "npc"),
+    ## height = unit(1, "grobheight", hl.subtitle) + 0.4*unit(1/(fsratio), "grobheight", hl.title),
+    height = unit(1, "grobheight", hl.subtitle) + unit(0.02, "npc"),
     gp=gpar(fill="white", col="black", lwd=1, cex=fsratio*scale)
   )
   grobTree(hl.title.bkg, hl.title, hl.box, hl.subtitle)
@@ -97,9 +103,9 @@ rc = quant$quant.expr.add(
 ##
 
 ##--- plot height 6cm
-my.graph.height = 6/2.54
+my.graph.height = 8/2.54
 ##--- plot width = 3/2 plot height
-my.graph.width= my.graph.height*1.5
+my.graph.width = my.graph.height*1.5
 ##--- set desired horizontal pixels for a plot
 my.graph.horiz.pixels = 560
 
@@ -143,7 +149,7 @@ plot.df = data.frame(
   Bl.min = quant$val("Be_lept") - quant$err("Be_lept"),
   Bl.max = quant$val("Be_lept") + quant$err("Be_lept"))
 
-rc = ggplot(data = plot.df, aes(x = tau, y = Bl)) + 
+rc = ggplot(data = plot.df, aes(x = tau, y = Bl)) +
   theme_bw() +
   coord_cartesian(xlim = c(xlim.min, xlim.max), ylim = c(ylim.min, ylim.max))
 
@@ -160,7 +166,7 @@ rm(rc2)
 rc = rc +
   geom_errorbar(aes(ymin=Bl.min,  ymax=Bl.max),  color="Red",
                 width=wisker.size.x,
-                size=0.6666) + 
+                size=0.6666) +
   geom_errorbarh(aes(xmin=tau.min, xmax=tau.max), color="Red",
                  height=wisker.size.y,
                  size=0.6666) +
@@ -174,7 +180,7 @@ rc = rc +
   ## labs(title="Lepton Universality Test") +
   labs(x=TeX("$\\tau_{\\tau}$(fs)"), y=TeX("$\\textit{B}_{\\tau e}^{\\prime}$")) +
   annotation_custom(hfag.label())
-  
+
 print(rc)
 
 fname = sub("[.][^.]*$", "", basename(opts$file))
